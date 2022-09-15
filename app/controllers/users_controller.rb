c13899d71b7f0c98b1d1d9575b5e778e87708be9
Wade Naughton <wadenaughton@gmail.com>
2022-09-15 17:42:43 -0400

    profile pic, trip participants

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+      params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :avatar)
     end
 end
