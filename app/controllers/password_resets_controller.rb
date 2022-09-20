f8ece81d02a2259be9bf47d18a082aa141d4b67b
Wade Naughton <wadenaughton@gmail.com>
2022-09-20 19:27:01 -0400

    email delivery test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,11 +3,23 @@ class PasswordResetsController < ApplicationController
   end
 
 
-  def create
+  # def create
+  #   user = User.find_by(email: params[:email]) && User.find_by(username: params[:username])
+  #   if user.present?
+  #     test = ApplicationMailer.with(user: user).forgot_password(user)
+  #     test.deliver_now
+  #     flash[:notice] = 'E-mail sent with password reset instructions.'
+  #     redirect_to "/"
+  #   else
+  #     flash[:notice] = 'invalid email or username'
+  #     render :new
+  #   end
+  # end
+
+    def create
     user = User.find_by(email: params[:email]) && User.find_by(username: params[:username])
     if user.present?
-      test = ApplicationMailer.with(user: user).forgot_password(user)
-      test.deliver_now
+      user.send_password_reset if user
       flash[:notice] = 'E-mail sent with password reset instructions.'
       redirect_to "/"
     else
@@ -26,6 +38,7 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find_by_password_reset_token!(params[:id])
+    binding.pry
     if @user.password_reset_sent_at < 2.hour.ago
       flash[:notice] = 'Password reset has expired'
       redirect_to new_password_reset_path
