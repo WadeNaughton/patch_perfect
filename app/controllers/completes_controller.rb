73d69f949006283b419a6f6989b81e0f175f37a5
Wade Naughton <wadenaughton@gmail.com>
2023-03-06 14:13:43 -0500

    complete route, header margin, remove leader

--- app/controllers/completes_controller.rb
+++ app/controllers/completes_controller.rb
@@ -15,6 +15,9 @@ class CompletesController < ApplicationController
     @hike = Hike.find(params[:id])
     @user = User.find_by(id: params[:user_id])
     complete = Complete.find_or_create_by(hike_id: @hike.id, user_id: @user.id)
+    if @hike.save
+      redirect_to "/users/#{@user.id}"
+    end
   end
 
 end
