ebf68283596d8c3b6d146419ccecac9728bf5e28
Wade Naughton <wadenaughton@gmail.com>
2023-03-16 11:46:46 -0400

    test

--- db/migrate/20230316024742_remove_column.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumn < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :user_id, :comments
-  end
-end
--- db/migrate/20230316032824_remove_participant_references_from_comments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveParticipantReferencesFromComments < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :comments, :participant_references, :string
-  end
-end
